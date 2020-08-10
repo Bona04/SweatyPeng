e8ad0611f4b7417609f80438b49cecec138245f1
Bona04 <60744262+Bona04@users.noreply.github.com>
2020-08-11 02:27:07 +0900

    일단 저장
    뱀이동, 뱀애니, 하트 스프라이트 추가

---
+++ Assets/Scripts/SnakeMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    CapsuleCollider2D capsuleCollider;
+
+    public int nextMove;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+
+        Invoke("Think", 5); //5초 뒤에 호출
+    }
+
+    void Update()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);   //기본 움직임
+
+        //지형 체크
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.3f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        if (rayHit.collider == null)
+        {
+            //Debug.Log("경고! 이 앞 낭떠러지.");
+            Turn();
+        }
+    }
+    void Think() //재귀함수
+    {
+        //Set Next Active
+        nextMove = Random.Range(-1, 2); //최소값, 최대값-1
+
+
+        //Flip Sprite
+        if (nextMove != 0)
+            spriteRenderer.flipX = nextMove == 1;
+
+        //Recursive
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+    void Turn()
+    {
+        nextMove = nextMove * -1; //방향 정반대로 바꿔줌.
+        spriteRenderer.flipX = nextMove == 1;
+        CancelInvoke();
+        Invoke("Think", 2);
+    }
+}
