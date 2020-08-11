ec61a39d64ac55b1cc648ca712ee542bd28f5be1
Bona04 <60744262+Bona04@users.noreply.github.com>
2020-08-12 01:29:36 +0900

    기본 공격을 넣었는데
    왼쪽으로 쏘는 법을 모르겠음.. 공부하고 돌아오겠다..

---
+++ Assets/Scripts/BuriAttack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuriAttack : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        Invoke("DestroyBullet", 1);
+    }
+
+
+    void Update()
+    {
+        if (transform.rotation.y == 0)
+            transform.Translate(transform.right * speed * Time.deltaTime);
+
+        else
+            transform.Translate(transform.right * -1 * speed * Time.deltaTime);
+    }
+
+    void DestroyBullet() // 소멸
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject attack;
+    public Transform pos;
+    public float cooltime;
+    private float curtime;
+    Animator anime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anime = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (curtime <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Z)) //기본공격
+            {
+                anime.SetTrigger("Attack");
+                Instantiate(attack, pos.position, transform.rotation);
+
+                curtime = cooltime;
+            }
+        }
+        curtime -= Time.deltaTime;
+
+    }
+}
