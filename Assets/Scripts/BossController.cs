e198cf4ebdac537b972bf80bc16a47d791b99545
Bona04 <60744262+Bona04@users.noreply.github.com>
2020-09-01 09:30:14 +0900

    땀,기본공격 1차
    땀이 일단 한쪽만 적용, 기본공격 1차로 대충 시도(Q키 누르는 걸로 가능)

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    //Animator anime;
+    public float cooltime;
+    private float curtime;
+    public GameObject BossNormalAttack;
+    public Transform BossAttackPos;
+
+    void Start()
+    {
+        //anime = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (curtime <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Q)) //기본공격
+            {
+                //anime.SetTrigger("NormalAttack");
+                Instantiate(BossNormalAttack, BossAttackPos.position, transform.rotation);
+                curtime = cooltime;
+            }
+        }
+        curtime -= Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/BossNormalBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossNormalBullet : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(transform.right * -1 * speed * Time.deltaTime);
+        if (transform.position.x < 100)
+            DestroyBullet();
+    }
+    void DestroyBullet() // 소멸
+    {
+        Destroy(gameObject);
+    }
+}
