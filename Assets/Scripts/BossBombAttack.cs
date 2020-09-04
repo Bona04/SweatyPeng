ed858bfc764f8f64c6cc3ac6eed8bd0aa5b22c66
Yubin Yang <yubin000217@naver.com>
2020-09-04 19:38:54 +0900

    폭탄 공격 구현 끝
    와아... 진짜.. 이거느은...

---
+++ Assets/Scripts/BossBombAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBombAttack : MonoBehaviour
+{
+    public GameObject explosionParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Vector3.forward * Time.deltaTime * 2);
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform" || collision.gameObject.tag == "Water")
+        {
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            Destroy(gameObject);
+
+        }
+    }
+}
--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -10,6 +10,11 @@ public class BossController : MonoBehaviour
     public GameObject BossNormalAttack;
     public Transform BossAttackPos;
 
+    public GameObject BombAttack;
+    public GameObject BombAim;
+
+    private float randomXvalueCopy;
+
     void Start()
     {
         //anime = GetComponent<Animator>();
@@ -27,5 +32,27 @@ public class BossController : MonoBehaviour
             }
         }
         curtime -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            float randomXvalue = Random.Range(100, 114);
+            randomXvalueCopy = randomXvalue;
+
+            StartCoroutine("BombAttackMethod");
+        }
+    }
+    IEnumerator BombAttackMethod()
+    {
+        InvokeBombAim();
+        yield return new WaitForSeconds(2);
+        InvokeBomb();
+    }
+    void InvokeBombAim ()
+    {
+        Instantiate(BombAim, new Vector3(randomXvalueCopy, -2.2f, 0.0f), BombAim.transform.rotation);
+    }
+    void InvokeBomb ()
+    {
+        Instantiate(BombAttack, new Vector3(randomXvalueCopy, 4.0f, 0.0f), BombAttack.transform.rotation);
     }
 }
