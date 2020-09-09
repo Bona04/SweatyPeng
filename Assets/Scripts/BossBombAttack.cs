88db69a3f8fa8f60c2560c3c4858d44a386ec62c
Bona04 <60744262+Bona04@users.noreply.github.com>
2020-09-10 00:42:14 +0900

    ..여기서 보스몹을 해결할 방법을 찾는중

--- Assets/Scripts/BossBombAttack.cs
+++ Assets/Scripts/BossBombAttack.cs
@@ -7,38 +7,20 @@ public class BossBombAttack : MonoBehaviour
     public GameObject explosionParticle;
     //private AudioSource bombAudio;//여기서 안할거면 의미 없는 코드;
     //public AudioClip bombExplosion;
-
-<<<<<<< HEAD
     void Start()
     {
         //bombAudio = GetComponent<AudioSource>();
-=======
-    private AudioSource bombAudio;
-
-    public AudioClip bombExplosion;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        bombAudio = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
->>>>>>> parent of 4cab7c7... 뭐 이것저것..
-
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Platform" || collision.gameObject.tag == "Water" || collision.gameObject.tag == "Player")
         {
-<<<<<<< HEAD
-           // bombAudio.PlayOneShot(bombExplosion, 1.0f);
 
-=======
-            bombAudio.PlayOneShot(bombExplosion, 1.0f);
->>>>>>> parent of 4cab7c7... 뭐 이것저것..
+           // bombAudio.PlayOneShot(bombExplosion, 1.0f);
             Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
             Destroy(gameObject);
 
