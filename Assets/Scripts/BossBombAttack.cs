c1e0e3d96dc34a6d5e96df4d3cc965b1e20c75ec
Bona04 <60744262+Bona04@users.noreply.github.com>
2020-09-10 00:22:14 +0900

    ..

--- Assets/Scripts/BossBombAttack.cs
+++ Assets/Scripts/BossBombAttack.cs
@@ -8,17 +8,37 @@ public class BossBombAttack : MonoBehaviour
     //private AudioSource bombAudio;//여기서 안할거면 의미 없는 코드;
     //public AudioClip bombExplosion;
 
+<<<<<<< HEAD
     void Start()
     {
         //bombAudio = GetComponent<AudioSource>();
+=======
+    private AudioSource bombAudio;
+
+    public AudioClip bombExplosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bombAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+>>>>>>> parent of 4cab7c7... 뭐 이것저것..
 
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Platform" || collision.gameObject.tag == "Water" || collision.gameObject.tag == "Player")
         {
+<<<<<<< HEAD
            // bombAudio.PlayOneShot(bombExplosion, 1.0f);
 
+=======
+            bombAudio.PlayOneShot(bombExplosion, 1.0f);
+>>>>>>> parent of 4cab7c7... 뭐 이것저것..
             Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
             Destroy(gameObject);
 
