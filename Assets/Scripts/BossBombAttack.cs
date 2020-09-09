b004b6305816736e83829213f27e8f9ef4e98768
Bona04 <60744262+Bona04@users.noreply.github.com>
2020-09-09 23:54:09 +0900

    ..

--- Assets/Scripts/BossBombAttack.cs
+++ Assets/Scripts/BossBombAttack.cs
@@ -5,27 +5,13 @@ using UnityEngine;
 public class BossBombAttack : MonoBehaviour
 {
     public GameObject explosionParticle;
+    //private AudioSource bombAudio;//여기서 안할거면 의미 없는 코드;
+    //public AudioClip bombExplosion;
 
-<<<<<<< HEAD
-
-   //private AudioSource bombAudio;//여기서 안할거면 의미 없는 코드;
-
-
-//    public AudioClip bombExplosion;
-=======
-    private AudioSource bombAudio;
-
-    public AudioClip bombExplosion;
->>>>>>> parent of 4cab7c7... 뭐 이것저것..
 
     void Start()
-<<<<<<< HEAD
-    { 
-        //bombAudio = GetComponent<AudioSource>();
-=======
     {
-        bombAudio = GetComponent<AudioSource>();
->>>>>>> parent of 4cab7c7... 뭐 이것저것..
+        //bombAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -36,13 +22,8 @@ public class BossBombAttack : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Platform" || collision.gameObject.tag == "Water" || collision.gameObject.tag == "Player")
-<<<<<<< HEAD
-        { 
-            //bombAudio.PlayOneShot(bombExplosion, 1.0f);
-=======
         {
-            bombAudio.PlayOneShot(bombExplosion, 1.0f);
->>>>>>> parent of 4cab7c7... 뭐 이것저것..
+            //bombAudio.PlayOneShot(bombExplosion, 1.0f);
             Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
             Destroy(gameObject);
 
